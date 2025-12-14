14ca5d0e12abb75ca5835ee720f1208a796d5d18
Dengel456867 <arthur.eymard@orange.fr>
2025-12-14 22:02:10 +0100

    Hypothetical movement: can reposition until attack/end turn (v0.1.45)

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,3 @@
 // Version de l'application - synchronisee avec package.json
 // Cette version est mise a jour par le script de deploiement
-export const APP_VERSION = '0.1.44';
+export const APP_VERSION = '0.1.45';
